acf88cb74f6d3a541d7c03090b3f40a555278cbe
Michael Iwanek <miwanek11199@gmail.com>
2023-01-02 14:10:09 -0800

    working on product cards - productforlistcomponent.js

--- frontend/src/components/ProductForListComponent.js
+++ frontend/src/components/ProductForListComponent.js
@@ -1,18 +1,29 @@
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Row, Col } from "react-bootstrap";
 
 const ProductForListComponent = () => {
   // https://react-bootstrap.netlify.app/components/cards/#rb-docs-content
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="/images/tablets-category.png" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+    <Card style={{ marginTop: "30px", marginBottom:"50px" }}>
+      {/* default width of row is 12: 5 + 7 = 12 */}
+      <Row>
+        <Col lg={5}>
+          <Card.Img variant="top" src="/images/tablets-category.png" />
+        </Col>
+        <Col lg={7}>
+        <Card.Body>
+        <Card.Title>Product Name Lorem Ipsum</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+        perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+        totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta 
+        sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia 
+        consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem 
+        ipsum quia dolor sit amet, consectetur, adipisc
+        </Card.Text>        
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
+        </Col>
+      </Row>
+
     </Card>
   );
 };
