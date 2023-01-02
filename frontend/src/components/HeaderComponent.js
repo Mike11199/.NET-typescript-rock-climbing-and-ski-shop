ec4eb6456f1cd48e3b7b5acaec47680893a081c7
Michael Iwanek <miwanek11199@gmail.com>
2023-01-01 19:59:07 -0800

    added more header links, dropdown menu, bootstrap icons, etc.

--- frontend/src/components/HeaderComponent.js
+++ frontend/src/components/HeaderComponent.js
@@ -1,55 +1,59 @@
 import { Navbar, Nav, Container, NavDropdown, Badge, Form, Dropdown, DropdownButton, Button, InputGroup } from "react-bootstrap";
+import { LinkContainer } from 'react-router-bootstrap' //makes a react bootstrap element behave like a react router link 
+import { Link } from 'react-router-dom'
 
 const HeaderComponent = () => {
   return (
     <>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          
-          <Navbar.Brand href="#home">Online Shop</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand href="/">Rock Climbing & Ski Shop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             
             <Nav className="me-auto">
                 <InputGroup>
-                    <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                    <DropdownButton id="dropdown-basic-button" title="All">
+                        <Dropdown.Item>Ropes</Dropdown.Item>
+                        <Dropdown.Item>Skis</Dropdown.Item>
+                        <Dropdown.Item>Harnesses</Dropdown.Item>
                     </DropdownButton>
-                    <Form.Control type="text" placeholder="Normal text" />
-                    <Button variant="warning">Warning</Button>{' '}
+                    <Form.Control type="text" placeholder="Search in shop..." />
+                    <Button variant="warning">
+                        <i className="bi bi-search text-dark"></i>
+                    </Button>
                 </InputGroup>
             </Nav>
 
             <Nav>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <Nav.Link href="#pricing">
-              <Badge pill bg="danger">
-                2
-              </Badge>
-              Cart
+            <LinkContainer to="/admin/orders">
+              <Nav.Link>
+                Admin
+                <span className="position-absolute top-1 start-10 translate-middle p-2 bg-danger border border-light rounded-circle"></span>
               </Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
+            </LinkContainer>
+              
+              <Nav.Link href="#pricing">
+                <Badge pill bg="danger">
+                    2
+                </Badge>
+                Cart
+              </Nav.Link>
+              <NavDropdown title="John Doe" id="collapsible-nav-dropdown">
+                <NavDropdown.Item eventKey="/user/my-orders" as={Link} to="/user/my-orders">My orders</NavDropdown.Item>
+                <NavDropdown.Item eventKey="/user/" as={Link} to="/user">My profile</NavDropdown.Item>
+                <NavDropdown.Item>Logout</NavDropdown.Item>
+
+
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
-  );
-};
+  )
+}
 
 export default HeaderComponent;
