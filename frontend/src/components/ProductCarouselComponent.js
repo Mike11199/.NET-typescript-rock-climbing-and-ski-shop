ae003c1fcdb82384bc6e82c951756ed1f998b910
Michael Iwanek <miwanek11199@gmail.com>
2023-01-01 21:41:14 -0800

    modified home page to show grid of category card components taking advantage of react bootstrap features.  passed props to card component from temp array until back end database set up

--- frontend/src/components/ProductCarouselComponent.js
+++ frontend/src/components/ProductCarouselComponent.js
@@ -1,42 +1,58 @@
 import {Carousel} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
 
 const ProductCarouselComponent = () => {
+  
+  const cursorP = {
+    cursor:"pointer"
+  }
+
   return (
     <Carousel>
       <Carousel.Item>
         <img
+          crossOrigin="anonymous"
           className="d-block w-100"
+          style={{height: "300px", objectFit:'cover'}}
           src="/images/carousel/carousel-1.png"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <LinkContainer style={cursorP} to="/product-details">
+          <h3>Bestseller in Harnesses Category</h3>
+          </LinkContainer>          
+          <p>Climbing Harness Big Wall 5000</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
+          style={{height: "300px", objectFit:'cover'}}
           src="/images/carousel/carousel-2.png"
           alt="Second slide"
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Bestseller in Books Category</h3>
+          </LinkContainer>  
+          <p>Freedom of the Hills</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
+          style={{height: "300px", objectFit:'cover'}}
           src="/images/carousel/carousel-3.png"
           alt="Third slide"
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Bestseller in Cameras Category</h3>
+          </LinkContainer>  
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            4k Camera
           </p>
         </Carousel.Caption>
       </Carousel.Item>
