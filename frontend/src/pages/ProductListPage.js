3f9b2c0201e023c93cbb307680008482b6fdee2b
Michael Iwanek <miwanek11199@gmail.com>
2023-01-02 10:31:11 -0800

    working on product list side panel

--- frontend/src/pages/ProductListPage.js
+++ frontend/src/pages/ProductListPage.js
@@ -1,6 +1,32 @@
+import { Row, Col, Container, ListGroup, Button } from "react-bootstrap";
+
 const ProductListPage = () => {
 
-    return <p>product list page</p>
+    return (
+        <Container fluid>
+        <Row>
+          <Col md={3}>
+            <ListGroup variant="flush">
+              <ListGroup.Item>test</ListGroup.Item>
+              <ListGroup.Item>test</ListGroup.Item>
+              <ListGroup.Item>test</ListGroup.Item>
+              <ListGroup.Item>test</ListGroup.Item>
+              <ListGroup.Item>
+                test
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button variant="primary">Primary</Button>
+                <Button variant="danger">Danger</Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+          <Col md={9}>
+            test
+            test
+          </Col>
+        </Row>
+      </Container>
+    )
 }
 
 export default ProductListPage
