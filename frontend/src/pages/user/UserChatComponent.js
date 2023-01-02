59fdff037fd31edaaf3c4daeabea012d1653d377
Michael Iwanek <miwanek11199@gmail.com>
2023-01-02 08:33:16 -0800

    added css and input label for future chat functionality between admin and website users

--- frontend/src/pages/user/UserChatComponent.js
+++ frontend/src/pages/user/UserChatComponent.js
@@ -1,7 +1,16 @@
+import '../../chats.css'
 
 const UserChatComponent = () => {
 
-    return <p>user chat component</p>
+    return (
+        <>
+        <input type="checkbox" id="check" />
+        <label className="chat-btn" htmlFor="check">
+            <i className="bi bi-chat-dots comment"></i>
+            <i className="bi bi-x-circle close"></i>
+        </label>
+        </>
+    )
 }
 
 export default UserChatComponent
