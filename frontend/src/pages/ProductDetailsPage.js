50d68366a8c946a7cadcf26647706c2e7b00b219
Michael Iwanek <miwanek11199@gmail.com>
2023-01-02 16:11:05 -0800

    added structure of rows and columns for the product details page

--- frontend/src/pages/ProductDetailsPage.js
+++ frontend/src/pages/ProductDetailsPage.js
@@ -1,10 +1,27 @@
-import { useParams } from "react-router-dom"
+import { Row, Col, Container } from "react-bootstrap";
 
 const ProductDetailsPage = () => {
+  return (
+    <Container>
+      <Row className="mt-5">
+        <Col md={4}>Images</Col>
+        <Col md={8}>
+          <Row>
+            <Col md={8}>product name, price, description, rating</Col>
+            <Col md={4}>product status, quantity</Col>
+          </Row>
+          <Row>
+            <Col className="mt-5">
+              <h5>REVIEWS</h5>
+            </Col>
+          </Row>
+          <hr />
+          send review form
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-    const {id} = useParams()
-    console.log(id)
-    return <p>product details page</p>
-}
+export default ProductDetailsPage;
 
-export default ProductDetailsPage
