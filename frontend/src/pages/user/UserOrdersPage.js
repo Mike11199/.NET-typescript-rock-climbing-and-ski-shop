0533ac71786c8e6505c5d0e6730a323e8e5344f4
Michael Iwanek <miwanek11199@gmail.com>
2023-01-02 22:14:45 -0800

    added bootstrap deafult table to userorderspage components as tempalte - finish later

--- frontend/src/pages/user/UserOrdersPage.js
+++ frontend/src/pages/user/UserOrdersPage.js
@@ -1,6 +1,44 @@
+import { Row, Col, Table } from "react-bootstrap";
+
 const UserOrdersPage = () => {
+  return (
+    // copied originally from here:  https://react-bootstrap.netlify.app/components/table/#example
+    <Row className="m-5">
+      <Col md={12}>
+        <h1>My Orders</h1>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td colSpan={2}>Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
+  );
+};
 
-    return <p>user orders page </p>
-}
+export default UserOrdersPage;
 
-export default UserOrdersPage
