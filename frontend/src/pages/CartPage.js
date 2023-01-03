89336add152955bc6ec8fbc9237291411de8338e
Michael Iwanek <miwanek11199@gmail.com>
2023-01-02 20:12:32 -0800

    finished placeholder for cart page until back end set up using bootstrap lisgroups, rows/cols, and forms

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,6 +1,39 @@
+import {Row, Col, Container, Alert, ListGroup, Button} from 'react-bootstrap'
+import { LinkContainer } from "react-router-bootstrap";
+import CartItemComponent from '../components/CartItemComponent';
+
 const CartPage = () => {
 
-    return <p>cart page</p>
+    return (
+        <Container fluid>
+        <Row className="mt-4">
+            <Col md={8}>
+                <h1>Shopping Cart</h1>
+                {Array.from({length: 3}).map((item, idx) =>(
+                    <>
+                        <CartItemComponent key={idx} /> 
+                    </>
+                ))}
+                <Alert variant="info">Your cart is empty</Alert>
+            </Col>
+            <Col md={4}>
+                <ListGroup>
+                    <ListGroup.Item>
+                        <h3>Subtotal (2 items)</h3>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        Price <span className='fw-bold'> $895</span>
+                    </ListGroup.Item>
+                    <LinkContainer to="/user/order-details">
+                        <ListGroup.Item>
+                            <Button>Proceed to Checkout</Button>
+                        </ListGroup.Item>  
+                    </LinkContainer>                                      
+                </ListGroup>
+            </Col>
+        </Row>
+        </Container>
+    )
 }
 
 export default CartPage
