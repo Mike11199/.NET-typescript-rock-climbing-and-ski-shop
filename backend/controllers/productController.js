ae06ec811cafc5267e587c305e121661042ee5b4
Michael Iwanek <miwanek11199@gmail.com>
2023-01-04 13:29:42 -0800

    modified product controller with get product by id, which uses mongodb populate function to populate the reviews array in the product schema, which contains each id only, with the actual review documentes from the other reviews collection

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,40 +4,54 @@ const recordsPerPage = require("../config/pagination");
 const getProducts = async (req, res, next) => {
   try {
     let query = {};
-    let queryCondition = false
+    let queryCondition = false;
 
     let priceQueryCondition = {};
     if (req.query.price) {
-        queryCondition = true;
+      queryCondition = true;
       priceQueryCondition = { price: { $lte: Number(req.query.price) } };
     }
     let ratingQueryCondition = {};
     if (req.query.rating) {
-        queryCondition = true;
+      queryCondition = true;
       ratingQueryCondition = { rating: { $in: req.query.rating.split(",") } };
     }
-    let categoryQueryCondition = {}
-    const categoryName = req.params.categoryName || ""
-    if(categoryName) {
-        queryCondition = true
-        let a = categoryName.replaceAll(",", "/")
-        var regEx = new RegExp("^" + a)
-        categoryQueryCondition = { category: regEx }
+    let categoryQueryCondition = {};
+    const categoryName = req.params.categoryName || "";
+    if (categoryName) {
+      queryCondition = true;
+      let a = categoryName.replaceAll(",", "/");
+      var regEx = new RegExp("^" + a);
+      categoryQueryCondition = { category: regEx };
     }
-    if(req.query.category) {
-        queryCondition = true
-        let a = req.query.category.split(",").map((item) => {
-            if(item) return new RegExp("^" + item)
-        })
-        categoryQueryCondition = {
-            category: { $in: a }
-        }
-    }
-
-    if (queryCondition) {
-      query = {
-        $and: [priceQueryCondition, ratingQueryCondition, categoryQueryCondition],
+    if (req.query.category) {
+      queryCondition = true;
+      let a = req.query.category.split(",").map((item) => {
+        if (item) return new RegExp("^" + item);
+      });
+      categoryQueryCondition = {
+        category: { $in: a },
       };
+    }
+    let attrsQueryCondition = [];
+    if (req.query.attrs) {
+      // attrs=RAM-1TB-2TB-4TB,color-blue-red
+      // [ 'RAM-1TB-4TB', 'color-blue', '' ]
+      attrsQueryCondition = req.query.attrs.split(",").reduce((acc, item) => {
+        if (item) {
+          let a = item.split("-");
+          let values = [...a];
+          values.shift(); // removes first item
+          let a1 = {
+            attrs: { $elemMatch: { key: a[0], value: { $in: values } } },
+          };
+          acc.push(a1);
+          // console.dir(acc, { depth: null })
+          return acc;
+        } else return acc;
+      }, []);
+    //   console.dir(attrsQueryCondition, { depth: null });
+    queryCondition = true;
     }
 
     //pagination
@@ -51,8 +65,33 @@ const getProducts = async (req, res, next) => {
       sort = { [sortOpt[0]]: Number(sortOpt[1]) };
     }
 
+    const searchQuery = req.params.searchQuery || ""
+    let searchQueryCondition = {}
+    let select = {}
+    if(searchQuery) {
+        queryCondition = true
+        searchQueryCondition = { $text: { $search: searchQuery } }
+        select = {
+            score: { $meta: "textScore" }
+        }
+        sort = { score: { $meta: "textScore" } }
+    }
+
+    if (queryCondition) {
+      query = {
+        $and: [
+          priceQueryCondition,
+          ratingQueryCondition,
+          categoryQueryCondition,
+          searchQueryCondition,
+          ...attrsQueryCondition
+        ],
+      };
+    }
+
     const totalProducts = await Product.countDocuments(query);
     const products = await Product.find(query)
+        .select(select)
       .skip(recordsPerPage * (pageNum - 1))
       .sort(sort)
       .limit(recordsPerPage);
@@ -66,5 +105,15 @@ const getProducts = async (req, res, next) => {
     next(error);
   }
 };
-module.exports = getProducts;
+
+const getProductById = async (req, res, next) => {
+    try {
+        const product = await Product.findById(req.params.id).populate("reviews").orFail()  //gets reviews by MongoDB id to review array
+        res.json(product)
+    } catch(err) {
+        next(err)
+    }
+}
+
+module.exports = {getProducts, getProductById};
 
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const getProducts = require("../controllers/productController")
+const {getProducts, getProductById} = require("../controllers/productController")
 
+router.get("/category/:categoryName/search/:searchQuery", getProducts)
 router.get("/category/:categoryName", getProducts)
+router.get("/search/:searchQuery", getProducts)
 router.get("/", getProducts)
+router.get("/:id", getProductById)
 
 module.exports = router
