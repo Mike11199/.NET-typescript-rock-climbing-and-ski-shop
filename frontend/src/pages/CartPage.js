f41b8468ac7aa43c68c49f7287e80b664c3f5161
Michael Iwanek <miwanek11199@gmail.com>
2023-01-03 18:48:37 -0800

    added cart details page

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,39 +1,39 @@
-import {Row, Col, Container, Alert, ListGroup, Button} from 'react-bootstrap'
+import { Container, Row, Col, Alert, ListGroup, Button } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
-import CartItemComponent from '../components/CartItemComponent';
+import CartItemComponent from "../components/CartItemComponent";
 
 const CartPage = () => {
+  return (
+    <Container fluid>
+      <Row className="mt-4">
+        <Col md={8}>
+          <h1>Shopping Cart</h1>
+          <ListGroup variant="flush">
+            {Array.from({ length: 3 }).map((item, idx) => (
+              <CartItemComponent key={idx} />
+            ))}
+          </ListGroup>
+          <Alert variant="info">Your cart is empty</Alert>
+        </Col>
+        <Col md={4}>
+          <ListGroup>
+            <ListGroup.Item>
+              <h3>Subtotal (2 Items)</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Price: <span className="fw-bold">$892</span>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <LinkContainer to="/user/cart-details">
+                <Button type="button">Proceed To Checkout</Button>
+              </LinkContainer>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-    return (
-        <Container fluid>
-        <Row className="mt-4">
-            <Col md={8}>
-                <h1>Shopping Cart</h1>
-                {Array.from({length: 3}).map((item, idx) =>(
-                    <>
-                        <CartItemComponent key={idx} /> 
-                    </>
-                ))}
-                <Alert variant="info">Your cart is empty</Alert>
-            </Col>
-            <Col md={4}>
-                <ListGroup>
-                    <ListGroup.Item>
-                        <h3>Subtotal (2 items)</h3>
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                        Price <span className='fw-bold'> $895</span>
-                    </ListGroup.Item>
-                    <LinkContainer to="/user/order-details">
-                        <ListGroup.Item>
-                            <Button>Proceed to Checkout</Button>
-                        </ListGroup.Item>  
-                    </LinkContainer>                                      
-                </ListGroup>
-            </Col>
-        </Row>
-        </Container>
-    )
-}
+export default CartPage;
 
-export default CartPage
