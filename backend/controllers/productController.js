7d6169a3bf2596ef083a17930c99423645a2faa5
Michael Iwanek <miwanek11199@gmail.com>
2023-01-04 13:51:03 -0800

    added API to retrieve bestsellers from the MongoDB database

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -108,12 +108,28 @@ const getProducts = async (req, res, next) => {
 
 const getProductById = async (req, res, next) => {
     try {
-        const product = await Product.findById(req.params.id).populate("reviews").orFail()  //gets reviews by MongoDB id to review array
+        const product = await Product.findById(req.params.id).populate("reviews").orFail()
         res.json(product)
     } catch(err) {
         next(err)
     }
 }
 
-module.exports = {getProducts, getProductById};
+const getBestsellers = async (req, res, next) => {
+    try {
+        const products = await Product.aggregate([
+            { $sort: { category: 1, sales: -1 } },
+            { $group: { _id: "$category", doc_with_max_sales: { $first: "$$ROOT" } } },
+            { $replaceWith: "$doc_with_max_sales" },
+            { $match: { sales: { $gt: 0 } } },
+            { $project: { _id: 1, name: 1, images: 1, category: 1, description: 1 } },
+            { $limit: 3 }
+        ])
+        res.json(products)
+    } catch(err) {
+        next(err)
+    }
+}
+
+module.exports = {getProducts, getProductById, getBestsellers};
 
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const {getProducts, getProductById} = require("../controllers/productController")
+const {getProducts, getProductById, getBestsellers} = require("../controllers/productController")
 
 router.get("/category/:categoryName/search/:searchQuery", getProducts)
 router.get("/category/:categoryName", getProducts)
 router.get("/search/:searchQuery", getProducts)
 router.get("/", getProducts)
+router.get("/bestsellers", getBestsellers)
 router.get("/:id", getProductById)
 
 module.exports = router
