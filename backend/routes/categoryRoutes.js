16127c32566fe95a4cb32963b694a8636a41451c
Michael Iwanek <miwanek11199@gmail.com>
2023-01-04 12:12:49 -0800

    added a save attribute API in category routes.  attrs is a key value pair in model with attibutes being list, ie colors: red, blue.  added logic in category controller to push new key/val if it doesn't exist, or push to key list of val if key does exist for category

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const {getCategories, newCategory} = require("../controllers/categoryController")
+const {getCategories, newCategory, deleteCategory, saveAttr} = require("../controllers/categoryController")
 
 router.get("/", getCategories)
 router.post("/", newCategory)
+router.delete("/:category", deleteCategory)
+router.post("/attr", saveAttr)
 
 module.exports = router
