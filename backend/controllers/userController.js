210b6a22cb3394750b0db339b246fece942bab2b
Michael Iwanek <miwanek11199@gmail.com>
2023-01-04 15:52:22 -0800

    finished API for log in user in userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 const User = require("../models/UserModel");
-const { hashPassword } = require("../utils/hashPassword");
-const generateAuthToken = require("../utils/generateAuthToken")
+const { hashPassword, comparePasswords } = require("../utils/hashPassword");
+const generateAuthToken = require("../utils/generateAuthToken");
 
 const getUsers = async (req, res, next) => {
   try {
@@ -30,11 +30,21 @@ const registerUser = async (req, res, next) => {
         password: hashedPassword,
       });
       res
-      .cookie("access_token", generateAuthToken(user._id, user.name, user.lastName, user.email, user.isAdmin), {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === "production",
-          sameSite: "strict"
-      })
+        .cookie(
+          "access_token",
+          generateAuthToken(
+            user._id,
+            user.name,
+            user.lastName,
+            user.email,
+            user.isAdmin
+          ),
+          {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+          }
+        )
         .status(201)
         .json({
           success: "User created",
@@ -52,5 +62,46 @@ const registerUser = async (req, res, next) => {
   }
 };
 
-module.exports = { getUsers, registerUser };
+const loginUser = async (req, res, next) => {
+  try {
+    const { email, password, doNotLogout } = req.body;
+    if (!(email && password)) {
+      return res.status(400).send("All inputs are required");
+    }
+
+    const user = await User.findOne({ email });
+    if (user && comparePasswords(password, user.password)) {
+      let cookieParams = {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+      };
+
+      if (doNotLogout) {
+        cookieParams = { ...cookieParams, maxAge: 1000 * 60 * 60 * 24 * 7 }; // 1000=1ms
+      }
+
+      return res.cookie(
+        "access_token",
+        generateAuthToken(
+          user._id,
+          user.name,
+          user.lastName,
+          user.email,
+          user.isAdmin
+        ),
+        cookieParams
+      ).json({
+          success: "user logged in",
+          userLoggedIn: { _id: user._id, name: user.name, lastName: user.lastName, email: user.email, isAdmin: user.isAdmin, doNotLogout }
+      });
+    } else {
+       return res.status(401).send("wrong credentials") 
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { getUsers, registerUser, loginUser };
 
