0b2dd7e66f64eeba132ecb648a8cf54b30b42189
Michael Iwanek <miwanek11199@gmail.com>
2023-01-04 12:54:22 -0800

    more work on product controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,13 +3,48 @@ const recordsPerPage = require("../config/pagination");
 
 const getProducts = async (req, res, next) => {
   try {
+    let query = {};
+    let queryCondition = false
+
+    let priceQueryCondition = {};
+    if (req.query.price) {
+        queryCondition = true;
+      priceQueryCondition = { price: { $lte: Number(req.query.price) } };
+    }
+    let ratingQueryCondition = {};
+    if (req.query.rating) {
+        queryCondition = true;
+      ratingQueryCondition = { rating: { $in: req.query.rating.split(",") } };
+    }
+
+    if (queryCondition) {
+      query = {
+        $and: [priceQueryCondition, ratingQueryCondition],
+      };
+    }
+
+    //pagination
     const pageNum = Number(req.query.pageNum) || 1;
-    // res.json({pageNum})
-    const products = await Product.find({})
+
+    // sort by name, price etc.
+    let sort = {};
+    const sortOption = req.query.sort || "";
+    if (sortOption) {
+      let sortOpt = sortOption.split("_");
+      sort = { [sortOpt[0]]: Number(sortOpt[1]) };
+    }
+
+    const totalProducts = await Product.countDocuments(query);
+    const products = await Product.find(query)
       .skip(recordsPerPage * (pageNum - 1))
-      .sort({ name: 1 })
+      .sort(sort)
       .limit(recordsPerPage);
-    res.json({ products });
+
+    res.json({
+      products,
+      pageNum,
+      paginationLinksNumber: Math.ceil(totalProducts / recordsPerPage),
+    });
   } catch (error) {
     next(error);
   }
