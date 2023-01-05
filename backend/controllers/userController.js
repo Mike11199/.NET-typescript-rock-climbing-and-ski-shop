f8cf3f11b74b4c6e59a0aa1c3c197ff034ce09d0
Michael Iwanek <miwanek11199@gmail.com>
2023-01-04 17:09:39 -0800

    added MongoDb transactions for the userController so that the mongodb operations can be reverted if a review is created but a user already created a review for that product

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,6 @@
 const User = require("../models/UserModel");
+const Review = require("../models/ReviewModel");
+const Product = require("../models/ProductModel");
 const { hashPassword, comparePasswords } = require("../utils/hashPassword");
 const generateAuthToken = require("../utils/generateAuthToken");
 
@@ -153,5 +155,61 @@ const getUserProfile = async (req, res, next) => {
     }
 }
 
-module.exports = { getUsers, registerUser, loginUser, updateUserProfile, getUserProfile };
+const writeReview = async (req, res, next) => {
+    try {
+
+        const session = await Review.startSession();
+
+        // get comment, rating from request.body:
+        const { comment, rating } = req.body;
+        // validate request:
+        if (!(comment && rating)) {
+            return res.status(400).send("All inputs are required");
+        }
+
+        // create review id manually because it is needed also for saving in Product collection
+        const ObjectId = require("mongodb").ObjectId;
+        let reviewId = ObjectId();
+
+        session.startTransaction();
+        await Review.create([
+            {
+                _id: reviewId,
+                comment: comment,
+                rating: Number(rating),
+                user: { _id: req.user._id, name: req.user.name + " " + req.user.lastName },
+            }
+        ],{ session: session })
+
+        const product = await Product.findById(req.params.productId).populate("reviews").session(session);
+        
+        const alreadyReviewed = product.reviews.find((r) => r.user._id.toString() === req.user._id.toString());
+        if (alreadyReviewed) {
+            await session.abortTransaction();
+            session.endSession();
+            return res.status(400).send("product already reviewed");
+        }
+
+        let prc = [...product.reviews];
+        prc.push({ rating: rating });
+        product.reviews.push(reviewId);
+        if (product.reviews.length === 1) {
+            product.rating = Number(rating);
+            product.reviewsNumber = 1;
+        } else {
+            product.reviewsNumber = product.reviews.length;
+            product.rating = prc.map((item) => Number(item.rating)).reduce((sum, item) => sum + item, 0) / product.reviews.length;
+        }
+        await product.save();
+
+        await session.commitTransaction();
+        session.endSession();
+        res.send('review created')
+    } catch (err) {
+        await session.abortTransaction();
+        next(err)   
+    }
+}
+
+module.exports = { getUsers, registerUser, loginUser, updateUserProfile, getUserProfile, writeReview };
 
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { verifyIsLoggedIn, verifyIsAdmin } = require("../middleware/verifyAuthToken");
-const {getUsers, registerUser, loginUser, updateUserProfile, getUserProfile} = require("../controllers/userController")
+const {getUsers, registerUser, loginUser, updateUserProfile, getUserProfile, writeReview} = require("../controllers/userController")
 
 router.post("/register", registerUser)
 router.post("/login", loginUser)
@@ -10,6 +10,7 @@ router.post("/login", loginUser)
 router.use(verifyIsLoggedIn);
 router.put("/profile", updateUserProfile);
 router.get('/profile/:id', getUserProfile)
+router.post('/review/:productId', writeReview)
 
 // admin routes:
 router.use(verifyIsAdmin);
