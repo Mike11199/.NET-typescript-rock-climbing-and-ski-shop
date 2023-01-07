dbf5a38ba6137c490bae2fc7fc58b8efe00db1e5
Michael Iwanek <miwanek11199@gmail.com>
2023-01-06 22:23:31 -0800

    fixed components so edit user now works correctly

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -226,7 +226,7 @@ const updateUser = async (req, res, next) => {
         user.name = req.body.name || user.name;
         user.lastName = req.body.lastName || user.lastName;
         user.email = req.body.email || user.email;
-        user.isAdmin = req.body.isAdmin || user.isAdmin;
+        user.isAdmin = req.body.isAdmin 
 
         await user.save();
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -62,7 +62,7 @@ function App() {
         {/* admin protected routes */}
         <Route element={<ProtectedRoutesComponent admin={true}/>}>
           <Route path="/admin/users" element={<AdminUsersPage />} />
-          <Route path="/admin/edit-user" element={<AdminEditUserPage />} />
+          <Route path="/admin/edit-user/:id" element={<AdminEditUserPage />} />
           <Route path="/admin/products" element={<AdminProductsPage />} />
           <Route path="/admin/create-new-product" element={<AdminCreateProductPage />} />
           <Route path="/admin/edit-product/:id" element={<AdminEditProductPage />} />
