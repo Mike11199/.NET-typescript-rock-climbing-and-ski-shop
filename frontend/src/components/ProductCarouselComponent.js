fe03b01c5902905c58255953b749d9b910c1acf2
Michael Iwanek <miwanek11199@gmail.com>
2023-01-07 10:29:18 -0800

    finished capability to show bestsellers in product carousel component

--- frontend/src/components/ProductCarouselComponent.js
+++ frontend/src/components/ProductCarouselComponent.js
@@ -1,63 +1,32 @@
-import {Carousel} from 'react-bootstrap'
-import {LinkContainer} from 'react-router-bootstrap'
+import { Carousel } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
-const ProductCarouselComponent = () => {
-  
+const ProductCarouselComponent = ({ bestSellers }) => {
   const cursorP = {
-    cursor:"pointer"
-  }
+    cursor: "pointer",
+  };
 
-  return (
+  return bestSellers.length > 0 ? (
     <Carousel>
-      <Carousel.Item>
-        <img
-          crossOrigin="anonymous"
-          className="d-block w-100"
-          style={{height: "300px", objectFit:'cover'}}
-          src="/images/carousel/carousel-1.png"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <LinkContainer style={cursorP} to="/product-details">
-          <h3>Bestseller in Harnesses Category</h3>
-          </LinkContainer>          
-          <p>Climbing Harness Big Wall 5000</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          style={{height: "300px", objectFit:'cover'}}
-          src="/images/carousel/carousel-2.png"
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-        <LinkContainer style={cursorP} to="/product-details">
-          <h3>Bestseller in Books Category</h3>
-          </LinkContainer>  
-          <p>Freedom of the Hills</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          style={{height: "300px", objectFit:'cover'}}
-          src="/images/carousel/carousel-3.png"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-        <LinkContainer style={cursorP} to="/product-details">
-          <h3>Bestseller in Cameras Category</h3>
-          </LinkContainer>  
-          <p>
-            4k Camera
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
+      {bestSellers.map((item, idx) => (
+        <Carousel.Item key={idx}>
+          <img
+            crossOrigin="anonymous"
+            className="d-block w-100"
+            style={{ height: "300px", objectFit: "cover" }}
+            src={item.images ? item.images[0].path : null}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <LinkContainer style={cursorP} to={`/product-details/${item._id}`}>
+              <h3>Bestseller in {item.category} Category</h3>
+            </LinkContainer>
+            <p>{item.description}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
     </Carousel>
-  );
+  ) : null;
 };
 
 export default ProductCarouselComponent;
--- frontend/src/pages/components/HomePageComponent.js
+++ frontend/src/pages/components/HomePageComponent.js
@@ -4,17 +4,23 @@ import { Row, Container } from "react-bootstrap";
 
 import { useEffect, useState } from "react";
 
-const HomePageComponent = ({ categories }) => {
+const HomePageComponent = ({ categories, getBestsellers }) => {
   
     const [mainCategories, setMainCategories] = useState([]);
+    const [bestSellers, setBestsellers] = useState([]);
 
     useEffect(() => {
+        getBestsellers()
+        .then((data) => {
+            setBestsellers(data);
+        })
+        .catch((er) => console.log(er.response.data.message ? er.response.data.message : er.response.data));
         setMainCategories((cat) => categories.filter((item) => !item.name.includes("/")));
     }, [categories])
 
   return (
     <>
-      <ProductCarouselComponent />
+      <ProductCarouselComponent bestSellers={bestSellers} />
       <Container>
         <Row xs={1} md={2} className="g-4 mt-5">
           {mainCategories.map((category, idx) => (
