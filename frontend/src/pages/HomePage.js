e8ff456f79fd379463d70f623c3431761dcd5dcb
Michael Iwanek <miwanek11199@gmail.com>
2023-01-06 22:52:41 -0800

    edited product details page to correctly show reviews and product info.  separated home page into two components

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,33 +1,9 @@
-import ProductCarouselComponent from "../components/ProductCarouselComponent"
-import CategoryCardComponent from "../components/CategoryCardComponent"
-import { Row, Container } from 'react-bootstrap'
+import HomePageComponent from "./components/HomePageComponent";
 
 const HomePage = () => {
 
-    const categories = [
-        "Tablets",
-        "Monitors",
-        "Games",
-        "Printers",
-        "Software",
-        "Cameras",
-        "Books",
-        "Videos",
-    ]
+  return <HomePageComponent />;
+};
 
-    return (
-    <>
-    {/* <p>home page</p> */}
-        <ProductCarouselComponent />
-        <Container>
-        <Row xs={1} md={2} className="g-4 mt-5">
-        {
-            categories.map( (category, idx)=> <CategoryCardComponent key={idx} category={category} idx={idx}/>)
-        }
-        </Row>
-        </Container>
-    </>        
-    )
-}
+export default HomePage;
 
-export default HomePage
