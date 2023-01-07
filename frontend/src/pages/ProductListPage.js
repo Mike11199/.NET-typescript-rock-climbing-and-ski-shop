4b04d009dd5171a566b14984783bc169dc0ccfd9
Michael Iwanek <miwanek11199@gmail.com>
2023-01-07 10:09:35 -0800

    modified product list page axios quest to dynamically build up URL string based on category, search, filters

--- frontend/src/pages/ProductListPage.js
+++ frontend/src/pages/ProductListPage.js
@@ -3,8 +3,16 @@ import axios from "axios";
 
 import { useSelector } from "react-redux";
 
+let filtersUrl = "";
+
 const getProducts = async (categoryName = "", pageNumParam = null, searchQuery = "", filters = {}, sortOption = "") => {
-    const { data } = await axios.get('/api/products');
+    //   filtersUrl = "&price=60&rating=1,2,3&category=a,b,c,d&attrs=color-red-blue,size-1TB-2TB";
+    filtersUrl = "";
+    console.log(filters);
+    const search = searchQuery ? `search/${searchQuery}/` : "";
+    const category = categoryName ? `category/${categoryName}/` : "";
+    const url = `/api/products/${category}${search}?pageNum=${pageNumParam}${filtersUrl}&sort=${sortOption}`;
+    const { data } = await axios.get(url);
     return data
 }
 
