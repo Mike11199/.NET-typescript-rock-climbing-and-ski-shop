c1b3d9124aa7a8c05c8c56a9876ff4804f07c234
Michael Iwanek <miwanek11199@gmail.com>
2023-01-08 19:24:23 -0800

    now client messages show on admin panel for chats

--- frontend/src/redux/reducers/adminChatReducers.js
+++ frontend/src/redux/reducers/adminChatReducers.js
@@ -7,10 +7,25 @@ const CHAT_INITIAL_STATE = {
 export const adminChatReducer = (state = CHAT_INITIAL_STATE, action) => {
     switch (action.type) {
         case actionTypes.SET_CHATROOMS:
-          return {
-              ...state,
-              chatRooms: { "to do:": "chatrooms for admin", [action.payload.user]: action.payload.message },
+          let currentState = { ...state };
+
+
+          //push client message to array
+          if (state.chatRooms[action.payload.user]) {
+              currentState.chatRooms[action.payload.user].push({ client: action.payload.message });
+              return {
+                 ...state, 
+                 chatRooms: { ...currentState.chatRooms },
+              }
+
           } 
+          //this part for if user writes for the first time
+          else {
+             return {
+                ...state,
+               chatRooms: { ...currentState.chatRooms, [action.payload.user]: [{ client: action.payload.message }] },  
+             } 
+          }
           default:
            return state;   
     }
