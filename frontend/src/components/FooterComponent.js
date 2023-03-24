16615182501e367e179117ede68ce93c2a1af04a
Michael Iwanek <miwanek11199@gmail.com>
2023-03-24 12:11:40 -0700

    quick change to footer

--- frontend/src/components/FooterComponent.js
+++ frontend/src/components/FooterComponent.js
@@ -8,7 +8,7 @@ const FooterComponent = () => {
         <Row className="mt-5">
           <Col className="bg-dark text-white text-center py-5">
               {/*  https://getbootstrap.com/docs/5.1/utilities/colors/  */}
-            Copyright &copy; Rock Climbing & Ski Shop
+            Copyright &copy; Rock Climbing & Ski Shop - Michael Iwanek
           </Col>
         </Row>
       </Container>
