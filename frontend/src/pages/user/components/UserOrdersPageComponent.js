b0a3afd9d68d12bc3ca862c58578a42cf342335d
Michael Iwanek <miwanek11199@gmail.com>
2023-04-02 23:26:32 -0700

    updated orders page to show all orders with items by default - option to hide order items

--- frontend/src/pages/user/components/UserOrdersPageComponent.js
+++ frontend/src/pages/user/components/UserOrdersPageComponent.js
@@ -1,28 +1,64 @@
-import { Row, Col, Table } from "react-bootstrap";
+
+import { Container, Row, Col, Form, Alert, ListGroup, Button, Table } from "react-bootstrap"
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import CartItemComponent from "../../../components/CartItemComponentSimple";
 
 const UserOrdersPageComponent = ({getOrders}) => {
     const [orders, setOrders] = useState([]);
     const {mode}  = useSelector((state) => state.DarkMode)
+    const [showDetails, setShowDetails] = useState({});
+
+    const dropDownStyle = {
+      slide: {
+          overflow: "hidden",
+          transitionProperty: "height",
+          transitionDuration: "400ms",
+          height: 0
+      },
+      slideOpen: {
+          overflow: "visible",
+          transitionProperty: "height",
+          transitionDuration: "400ms",
+      }
+  };
+
 
     useEffect(() => {
         getOrders()
         .then(orders => setOrders(orders))
-        .catch((er) => console.log(er));        
+        .catch((er) => console.log(er));     
     }, [])
+
+    useEffect(() => {
+      let defaultShowDetails = {};
+      for (let i = 0; i < orders.length; i++) {
+        defaultShowDetails[orders[i]._id] = true;
+      }
+      setShowDetails(defaultShowDetails);
+    }, [orders]);
+
 
     let USDollar = new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
     });
+
+    const toggleDetails = (orderId) => {
+      setShowDetails(prevShowDetails => ({
+        ...prevShowDetails,
+        [orderId]: !prevShowDetails[orderId]
+      }));
+    };
+    
     
     useEffect(() => { 
-    {console.log(orders)}
+      {console.log(orders)}
     },[orders])
 
   return (
+    <>
     <Row className="m-5">
       <Col md={12}>
         <h1>My Orders</h1>
@@ -36,8 +72,10 @@ const UserOrdersPageComponent = ({getOrders}) => {
               <th>Item Count</th>
               <th>Delivered</th>
               <th>Paid?</th>
-              <th>Payment Method</th>
-              <th>Order Details</th>              
+              <th>Payment Method</th>              
+              <th style={{ textAlign: 'center' }}>Order Details</th>  
+              <th style={{ textAlign: 'center' }}>Toggle Details</th>            
+              <th style={{width:'50%'}}>Order Items</th>              
             </tr>
           </thead>
           <tbody>
@@ -58,9 +96,27 @@ const UserOrdersPageComponent = ({getOrders}) => {
                   <td>
                    {order.paymentMethod === 'pp' ? 'PayPal' : 'Cash'}
                   </td>
-                  <td>
+                  <td style={{ textAlign: 'center' }}>
                     <Link to={`/user/order-details/${order._id}`}>View Order Details</Link>
                   </td>
+                  <td style={{ textAlign: 'center' }}>
+                    <Button variant="danger" onClick={() => toggleDetails(order._id)}>
+                      {showDetails[order._id] ? 'Hide Details' : 'Show Details'}
+                    </Button>
+                  </td>
+                  <>
+                  {showDetails.hasOwnProperty(order._id) && showDetails[order._id]  && (
+                    <td colSpan="2" >
+                    <ListGroup variant="flush" >
+                      {order.cartItems.map((item, idx) => (
+                        <div>
+                        <CartItemComponent item={item} key={idx} orderCreated={true}/>
+                        </div>
+                      ))}
+                    </ListGroup>
+                  </td>
+                  )}
+              </>
                 </tr>
               )
             )}
@@ -68,6 +124,7 @@ const UserOrdersPageComponent = ({getOrders}) => {
         </Table>
       </Col>
     </Row>
+    </>
   );
 };
 
