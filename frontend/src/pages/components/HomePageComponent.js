73af3f36e0a275bb766b454c9c29466514cc6672
Michael Iwanek <miwanek11199@gmail.com>
2023-06-11 22:29:26 -0700

    added outside library for clouds and css for them to move back and forth using translate and animate alternate

--- frontend/src/pages/components/HomePageComponent.js
+++ frontend/src/pages/components/HomePageComponent.js
@@ -3,6 +3,8 @@ import CategoryCardComponent from "../../components/CategoryCardComponent";
 import { Row, Container, Col } from "react-bootstrap";
 import Skier_Vector from "../../images/skier_vector.png"
 import Slope_Background from "../../images/slope_background2.png"
+import Snowfall from 'react-snowfall'
+import CloudVector from "../../images/cloud_vector2.png"
 
 import { useEffect, useState } from "react";
 
@@ -30,10 +32,24 @@ const HomePageComponent = ({ categories, getBestsellers }) => {
   return (
     <>
       <div className="ski_banner_box">
+      <Snowfall />
         {/* <div className="ski_slope"></div> */}
         <img className="skier_banner_slope" alt="skier_slope" src={Slope_Background} />
+        <img className="cloud_banner_vector" alt="cloud_vector" src={CloudVector} />
+        <img className="cloud_banner_vector2" alt="cloud_vector" src={CloudVector} />
+        <img className="cloud_banner_vector3" alt="cloud_vector" src={CloudVector} />
+        <img className="cloud_banner_vector4" alt="cloud_vector" src={CloudVector} />
+        <img className="cloud_banner_vector5" alt="cloud_vector" src={CloudVector} />
+        <img className="cloud_banner_vector6" alt="cloud_vector" src={CloudVector} />
+        <img className="cloud_banner_vector7" alt="cloud_vector" src={CloudVector} />
+        <img className="cloud_banner_vector8" alt="cloud_vector" src={CloudVector} />
+        <img className="cloud_banner_vector9" alt="cloud_vector" src={CloudVector} />
+        <img className="cloud_banner_vector10" alt="cloud_vector" src={CloudVector} />
+        <img className="cloud_banner_vector11" alt="cloud_vector" src={CloudVector} />
+        <img className="cloud_banner_vector12" alt="cloud_vector" src={CloudVector} />
+        <img className="cloud_banner_vector13" alt="cloud_vector" src={CloudVector} />
         <img className="skier_banner_vector" alt="skier_vector" src={Skier_Vector} />
-
+        
       </div>
 
 
