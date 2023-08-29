f0423558e4db31d3ec91e913a26188a557c2a5fa
Michael Iwanek <miwanek11199@gmail.com>
2023-08-28 23:30:47 -0700

    change footer to test ci cd pipeline and load balancer

--- frontend/src/components/FooterComponent.js
+++ frontend/src/components/FooterComponent.js
@@ -2,13 +2,16 @@
 import { Container, Row, Col } from "react-bootstrap";
 
 const FooterComponent = () => {
+
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer>
       <Container fluid>
         <Row className="mt-5">
           <Col className="bg-dark text-white text-center py-5">
               {/*  https://getbootstrap.com/docs/5.1/utilities/colors/  */}
-            Copyright &copy; Rock Climbing & Ski Shop - Michael Iwanek
+            Copyright {currentYear} &copy; Rock Climbing & Ski Shop - Michael Iwanek
           </Col>
         </Row>
       </Container>
