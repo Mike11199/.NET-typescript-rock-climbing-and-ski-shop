aa06338c923f52c58df0f76b154bf1b9131222e0
Michael Iwanek <miwanek11199@gmail.com>
2024-03-05 21:49:02 -0800

    edit route for v2 api

--- backend_v2_dotnet/Controllers/WeatherForecastController.cs
+++ backend_v2_dotnet/Controllers/WeatherForecastController.cs
@@ -1,33 +1,34 @@
-using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc;
 
-namespace backend_v2.Controllers
-{
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    namespace backend_v2.Controllers
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        [ApiController]
+        [Route("apiv2/[controller]")]
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public class WeatherForecastController : ControllerBase
         {
-            _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            private static readonly string[] Summaries = new[]
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+            private readonly ILogger<WeatherForecastController> _logger;
+
+            public WeatherForecastController(ILogger<WeatherForecastController> logger)
+            {
+                _logger = logger;
+            }
+
+            [HttpGet(Name = "GetWeatherForecast")]
+            public IEnumerable<WeatherForecast> Get()
+            {
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+            }
         }
     }
-}
