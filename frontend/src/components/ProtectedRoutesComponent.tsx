39d1cd09b5e1c79dbe831b817bab34f2dd9cebce
Michael Iwanek <miwanek11199@gmail.com>
2024-03-16 21:42:16 -0700

    Revert "apiv2 fix"
    This reverts commit ec0f3628f1603d75618f72424ee2ce51c1e8bb08.

--- frontend/src/components/ProtectedRoutesComponent.tsx
+++ frontend/src/components/ProtectedRoutesComponent.tsx
@@ -1,11 +1,10 @@
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
 import UserChatComponent from "../pages/user/UserChatComponent"
 
 import axios from "axios";
-import React, { useState, useEffect } from "react"
+import React, { useEffect, useState } from "react";
+import LoginPage from "../pages/LoginPage";
 import apiURL from "../../src/utils/ToggleAPI";
-
-
 
 interface AxiosGetTokenResponse {
     token?: any;
@@ -13,43 +12,39 @@ interface AxiosGetTokenResponse {
 }
 
 const ProtectedRoutesComponent = ({ admin }) => {
-
   const [isAuth, setIsAuth] = useState<any>();
-  const [isAdmin, setIsAdmin] = useState<any>();
-  const navigate = useNavigate()
 
-useEffect(() => {
-  axios.get<AxiosGetTokenResponse>(`${apiURL}/get-token`).then(function (response) {
-    if (response.data.isAdmin) {
-      setIsAuth(response.data.token);
-      setIsAdmin(response.data.isAdmin);
-    }
-    console.log(response.data.isAdmin)
-    console.log(response.data.token)
-  }).catch(function (error) {
-    console.error('Error fetching token:', error);
-    console.log('Error fetching token:', error);
-  });
-}, [isAuth, isAdmin])
-
-
+  useEffect(() => {
+      axios.get<AxiosGetTokenResponse>(`${apiURL}/get-token`).then(function (response) {
+      if (response.data.token) {
+        setIsAuth(response.data.token);
+      }
+    }).catch(function (error) {
+      console.error('Error fetching token:', error);
+      console.log('Error fetching token:', error);
+    });
+  }, [isAuth]);
 
   // if you are not authorized all then you have to log in.  return log in page.
- 
-
-  console.log(isAuth && admin && !isAdmin)
-  console.log(isAuth )
-  console.log(admin)
-  console.log(isAdmin)
+  if (isAuth === undefined) return <LoginPage />;
 
   // if you are authorized, but not an admin, and trying to access an admin page, go back to login page
-  return isAuth && admin && isAdmin ? (
+  return isAuth && admin && isAuth !== "admin" ? (
+       <Navigate to="/login" />
+
+  // else if admin and authorized return admin routes
+  ) : isAuth && admin ? (
       <Outlet />
-  ) : (
+
+  // else if not admin and authorized return user routes
+  ) : isAuth && !admin ? (
       <>
       <UserChatComponent />
       <Outlet />
       </>
+  // else navigate to login page
+  ) : (
+       <Navigate to="/login" />
   )
 };
 
