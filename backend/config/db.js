c3da0e5220aa5ecd0e39a08f8069575360337e62
Michael Iwanek <miwanek11199@gmail.com>
2024-03-29 19:45:25 -0700

    fix unhandled connection error that keeps crashing api MongoServerSelectionError

--- backend/config/db.js
+++ backend/config/db.js
@@ -19,4 +19,20 @@ const connectDB = async () => {
   }
 };
 
+  // Listen to connection events
+  mongoose.connection.on('connected', () => {
+    console.log('MongoDB connected!');
+  });
+
+  mongoose.connection.on('error', (err) => {
+    console.error(`MongoDB connection error: ${err}`);
+    mongoose.disconnect();
+  });
+
+  mongoose.connection.on('disconnected', () => {
+    console.log('MongoDB disconnected. Attempting to reconnect...');
+    setTimeout(connectDB, 5000);
+  });
+
+
 module.exports = connectDB;
