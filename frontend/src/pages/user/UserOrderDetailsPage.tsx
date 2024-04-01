9b210382bf2f91420722614a8f96b35a1fe4debe
Michael Iwanek <miwanek11199@gmail.com>
2024-03-31 22:36:44 -0700

    fix user route

--- frontend/src/pages/user/UserOrderDetailsPage.tsx
+++ frontend/src/pages/user/UserOrderDetailsPage.tsx
@@ -2,6 +2,7 @@ import UserOrderDetailsPageComponent from "./components/UserOrderDetailsPageComp
 import { useSelector } from "react-redux";
 import axios from 'axios'
 import { loadScript } from "@paypal/paypal-js"
+import { StoredUserInfo } from "types";
 
 
 // GET Request to retrieve order based on id in URL - error-handling in page component itself
@@ -107,11 +108,11 @@ const onErrorHandler = () => {
 const UserOrderDetailsPage = () => {
 
     //get user info from redux global state
-    const userInfo = useSelector((state: any) => state.userRegisterLogin.userInfo);
+    const userInfo = useSelector((state: any) => state.userRegisterLogin.userInfo as StoredUserInfo);
 
     //error-handling in page component itself
     const getUser = async () => {
-        const { data } = await axios.get("/apiv2/users/profile/" + userInfo._id);
+        const { data } = await axios.get("/apiv2/users/profile/" + userInfo.userId);
         return data;
     }
           //pass functions for API requests to component and state
