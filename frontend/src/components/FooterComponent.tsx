f72f158c5104a0d6c74114edc66d12a4f6e599c0
Michael Iwanek <91037796+Mike11199@users.noreply.github.com>
2024-12-25 18:03:12 -0800

    fix copyright symbol not by word copyright

--- frontend/src/components/FooterComponent.tsx
+++ frontend/src/components/FooterComponent.tsx
@@ -9,8 +9,7 @@ const FooterComponent = () => {
         <Row>
           <Col className="text-white text-center py-5">
             {/*  https://getbootstrap.com/docs/5.1/utilities/colors/  */}
-            Copyright {currentYear} &copy; Rock Climbing & Ski Shop - Michael
-            Iwanek
+            Copyright &copy; {currentYear} Rock Climbing & Ski Shop - Michael Iwanek
           </Col>
         </Row>
       </Container>
