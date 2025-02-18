dfa2ab2b863862d8121c4ff1aa36c2939c7d283b
Michael Iwanek <91037796+Mike11199@users.noreply.github.com>
2025-02-17 19:22:58 -0800

    fix bug - user is logged out when JWT expires

--- frontend/src/utils/AxiosSetup.tsx
+++ frontend/src/utils/AxiosSetup.tsx
@@ -1,6 +1,10 @@
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { logout } from "../redux/actions/userActions";
 
 const setupAxiosInterceptor = () => {
+  const dispatch = useDispatch();
+
   axios.interceptors.request.use(
     async (config) => {
       if (!config.headers) {
@@ -10,7 +14,30 @@ const setupAxiosInterceptor = () => {
       const getToken = async () => {
         let token =
           localStorage.getItem("token") || sessionStorage.getItem("token");
-        return token;
+
+        if (!token) {
+          return null;
+        }
+
+        try {
+          const payloadBase64 = token.split(".")[1];
+
+          if (!payloadBase64) {
+            console.error("Invalid token format");
+            return null;
+          }
+          const decodedPayload = JSON.parse(atob(payloadBase64));
+
+          if (!decodedPayload.exp || decodedPayload.exp * 1000 < Date.now()) {
+            console.log("Token is expired");
+            dispatch(logout());
+            return null;
+          }
+
+          return token;
+        } catch (error) {
+          return null;
+        }
       };
 
       const token = await getToken();
@@ -21,7 +48,7 @@ const setupAxiosInterceptor = () => {
 
       return config;
     },
-    (error) => Promise.reject(error),
+    (error) => Promise.reject(error)
   );
 };
 
