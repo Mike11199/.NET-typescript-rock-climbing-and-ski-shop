370c373ada0a526e1bb6972f00b720a5bc0c5d75
Michael Iwanek <91037796+Mike11199@users.noreply.github.com>
2025-03-21 21:53:55 -0700

    fix bug - img category card link on front page should use cursor pointer

--- frontend/src/components/CategoryCardComponent.tsx
+++ frontend/src/components/CategoryCardComponent.tsx
@@ -35,24 +35,25 @@ const CategoryCardComponent = ({ category, idx }) => {
 
   const handleClick = (categoryName) => {
     navigate(
-      `/product-list?pageNum=1&category=${encodeURIComponent(categoryName)}`,
+      `/product-list?pageNum=1&category=${encodeURIComponent(categoryName)}`
     );
   };
 
   return (
     <>
       <Card style={cardStyle as any}>
-        <LinkContainer
-          to={`/product-list?pageNum=1&category=${encodeURIComponent(category.name)}`}
-        >
-          <img
-            src={category.image ?? null}
-            height="365px"
-            key={category.name}
-            className={`category_card_image_front_page_${category.name.toLowerCase()}`}
-            alt="category"
-          ></img>
-        </LinkContainer>
+        <img
+          src={category.image ?? ""}
+          height="365px"
+          alt="category"
+          className={`category_card_image_front_page_${category.name.toLowerCase()}`}
+          style={{ cursor: "pointer" }}
+          onClick={() =>
+            navigate(
+              `/product-list?pageNum=1&category=${encodeURIComponent(category.name)}`
+            )
+          }
+        />
         <Card.Body style={{ touchAction: "manipulation", userSelect: "none" }}>
           <Card.Title style={{ userSelect: "text" }}>
             {category.name}
